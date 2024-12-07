031600e7eebb9335024e28b09e2f86a73cd04953
Zeina6699 <zenahhamad25@gmail.com>
2024-12-07 06:59:33 -0800

    create fromJson constructor

--- lib/Models/Weather_Model.dart
+++ lib/Models/Weather_Model.dart
@@ -1,9 +1,20 @@
 class WeatherModel{
-  final String date;
-  final double temp;
-  final double maxTemp;
-  final double minTemp;
-  final String weatherStateName;
+   String? date;
+   double? temp;
+   double? maxTemp;
+   double? minTemp;
+   String? weatherStateName;
 
   WeatherModel({required this.date, required this.temp, required this.maxTemp, required this.minTemp, required this.weatherStateName});
+  
+  WeatherModel.fromJson(dynamic data){
+    var json=data['forecast']['forecastday'][0]['day'];
+    date:data['location']['localtime'];
+     temp=json['avgtemp_c'];
+     maxTemp: json['maxtemp_c'];
+     minTemp: json['mintemp_c'];
+     weatherStateName:json['condition']['text'];
+    }
+
+
 }
--- lib/Services/Weather_Services.dart
+++ lib/Services/Weather_Services.dart
@@ -12,11 +12,15 @@ class WeatherService{
     Uri url=Uri.parse('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=7');
   http.Response response=await http.get(url);
   Map<String,dynamic> data =jsonDecode(response.body);
-  var jsonData=data['forecast']['forecastday'][0]['day'];
+
+WeatherModel weather=WeatherModel.fromJson(data);
+return weather;
+  
+  /*var jsonData=data['forecast']['forecastday'][0]['day'];
   WeatherModel weather=WeatherModel(date:data['location']['localtime'], temp:jsonData['avgtemp_c'],maxTemp: jsonData['maxtemp_c'], minTemp: jsonData['mintemp_c'],
    weatherStateName:jsonData['condition']['text']);
-   return weather;
- // print(data);
+   
+ // print(data);*/
 
   }
 
