843ada2aa700f2e4a5dc891fd2eff86b1151ecf7
Zeina6699 <zenahhamad25@gmail.com>
2024-12-07 09:30:14 -0800

    create factory constructor

--- lib/Models/Weather_Model.dart
+++ lib/Models/Weather_Model.dart
@@ -1,19 +1,29 @@
 class WeatherModel{
-   String? date;
-   double? temp;
-   double? maxTemp;
-   double? minTemp;
-   String? weatherStateName;
+   String date;
+   double temp;
+   double maxTemp;
+   double minTemp;
+   String weatherStateName;
 
   WeatherModel({required this.date, required this.temp, required this.maxTemp, required this.minTemp, required this.weatherStateName});
   
-  WeatherModel.fromJson(dynamic data){
-    var json=data['forecast']['forecastday'][0]['day'];
+  factory WeatherModel.fromJson(dynamic data){
+  var json=data['forecast']['forecastday'][0]['day'];
+
+  return WeatherModel(
+  date: data['location']['localtime'], 
+  temp: json['avgtemp_c'],
+  maxTemp:  json['maxtemp_c'],
+  minTemp:json['mintemp_c'],
+  weatherStateName:json['condition']['text']);
+
+
+   /* var json=data['forecast']['forecastday'][0]['day'];
     date:data['location']['localtime'];
      temp=json['avgtemp_c'];
      maxTemp: json['maxtemp_c'];
      minTemp: json['mintemp_c'];
-     weatherStateName:json['condition']['text'];
+     weatherStateName:json['condition']['text'];*/
     }
 
 
