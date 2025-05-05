916fcf257f5fd7fdc84225333321218a055990a9
Zeina6699 <zenahhamad25@gmail.com>
2025-05-05 06:46:51 -0700

    Finish weather App

--- lib/Screens/Splash_page.dart
+++ lib/Screens/Splash_page.dart
@@ -6,17 +6,25 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // الانتقال إلى الصفحة الرئيسية بعد ثانيتين
-    Future.delayed(Duration(seconds: 2), () {
+    Future.delayed(Duration(seconds:8), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => HomePage()),
       );
     });
 
     return Scaffold(
-      body: Center(
-        child: Image.asset(
-          'assets/images/pp.jpg', // استبدل هذا بمسار الصورة الخاصة بك
-          fit: BoxFit.cover,
+      body: Container(
+          decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 143, 198, 243),// استبدل بمسار الصورة الخاصة بك
+        
+          
+        ),
+        child: Center(
+          child: Image.asset(
+            width: 200,
+            'assets/images/k.gif', // استبدل هذا بمسار الصورة الخاصة بك
+            fit: BoxFit.contain,
+          ),
         ),
       ),
     );
